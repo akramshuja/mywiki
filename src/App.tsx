9bbfe2200d7ce6488baba23602084207b18c5a1a
akramshuja <78296491+akramshuja@users.noreply.github.com>
2025-07-26 15:37:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/mywiki/>
+      <BrowserRouter basename="/mywiki/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
